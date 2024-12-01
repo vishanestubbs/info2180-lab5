a319ef6f482259a7dd21aa78032c8405fcdf8a01
Vishane Stubbs <stubbsvishane@gmail.com>
2024-11-30 22:36:30 -0500

    Completed Exercise 4

--- world.js
+++ world.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     button.addEventListener("click", function () {
         const value = input.value.trim() // Get the input value
-        fetch(`http://localhost:8888/info2180-lab5/world.php?countryww=${encodeURIComponent(value)}`)
+        fetch(`http://localhost:8888/info2180-lab5/world.php?country=${encodeURIComponent(value)}`)
             .then(response => response.text())
             .then(data => results.innerHTML = data)  // Display the results in the result div
             .catch(error => alert(error));  // Display error if the fetch fails
